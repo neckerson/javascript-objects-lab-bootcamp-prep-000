295051a8533a1775c47a29748a4221996e25adc4
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-27 19:17:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+  object[key] = value;
+  return object;
+}
